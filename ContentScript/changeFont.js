df48aac5def95713a420bf76da590e3ba7517dea
Low Ming Lim <48002577+Avalionnet@users.noreply.github.com>
2022-01-08 02:37:56 +0800

    Link font type

--- ContentScript/changeFont.js
+++ ContentScript/changeFont.js
@@ -1,49 +1,55 @@
-function changeFont(fontNo) {
+console.log("change Font activated!!")
+
+chrome.runtime.onMessage.addListener(changeFont);
+
+function changeFont(message, sender, sendResponse) {
+    console.log(typeof message);
+    fontNo = message
     allElements = document.querySelectorAll('*');
     switch(fontNo) {
-    case 1:
+    case "1":
         // Arial
         for (items of allElements) {
             items.style["font-family"] = 'Arial';
         }
         break;
-    case 2:
+    case "2":
         // Comic Sans
         for (items of allElements) {
             items.style["font-family"] = 'Comic Sans';
         }
         break;
-    case 3:
+    case "3":
         // Calibri
         for (items of allElements){
             items.style["font-family"] = 'Calibri';
         }
         break;
-    case 4:
+    case "4":
         // Verdana
         for (items of allElements){
             items.style["font-family"] = 'Verdana';
         }
         break;
-    case 5:
+    case "5":
         // Tahoma
         for (items of allElements){
             items.style["font-family"] = 'Tahoma';
         }
         break;
-    case 6:
+    case "6":
         // Open Sans
         for (items of allElements){
             items.style["font-family"] = 'Open Sans';
         }
         break;
-    case 7:
+    case "7":
         // Roboto 
         for (items of allElements){
             items.style["font-family"] = 'Roboto';
         }
         break;
-    case 8:
+    case "8":
         // Helvetica
         for (items of allElements){
             items.style["font-family"] = 'Helvetica';
@@ -54,4 +60,4 @@ function changeFont(fontNo) {
     }
 }
 
-changeFont(1);
+// changeFont(6);
