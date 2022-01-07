541b1b4ff986f47219b7c0f57f2ea444b80ad6b9
Si Ting <33442908+SiTingST@users.noreply.github.com>
2022-01-08 00:18:09 +0800

    add light and dark mode

--- PopUp/popup.js
+++ PopUp/popup.js
@@ -2,21 +2,19 @@ function feedbackResponse() {
     return alert('Thank you for your response!')
 }
 
+console.log("here i am")
+
 const darkModeToggleButton = document.querySelector('#darkModeCheckBox')
 
 darkModeToggleButton,addEventListener("click", () => {
 
     if(document.querySelector('#darkModeCheckBox:checked')) {
       chrome.tabs.executeScript({
-          file:"../contentScript/darkmode.js"
+          file:"../contentScript/darkMode.js"
       })
     } else{
-        document.querySelector("html").style.filter="invert(0) hue-rotate(0deg)";
-    
-        let media = document.querySelectorAll("img, pictures, video");
-        
-        media.forEach((mediaItem)=>{
-            mediaItem.style.filter = "invert(0) hue-rotate(0deg)"
+        chrome.tabs.executeScript({
+            file:"../contentScript/lightMode.js"
         })
     }
 
