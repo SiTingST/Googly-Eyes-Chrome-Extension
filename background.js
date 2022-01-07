9e9edbb74286ea46208172a96a741528d2084c5a
Kexin Yeo <kexinyeo@Kexins-MacBook-Pro.local>
2022-01-07 16:06:16 +0800

    Change font size feature

--- background.js
+++ background.js
@@ -1,6 +1,13 @@
 let color = '#3aa757';
+let size = 100;
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  chrome.storage.sync.set({ size });
+
+  chrome.contextMenus.create({
+    id: "readText",
+    title: "Read selected text: %s", 
+    contexts:[ "selection" ]
+  });
 });
--- popup.js
+++ popup.js
@@ -1,17 +1,61 @@
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+// document.addEventListener('mouseup', event => {  
+//   if(window.getSelection().toString().length){
+//      let exactText = window.getSelection().toString();     
+//      console.log(exactText);   
+//   }
+// });
+
+fontSizeSlider.addEventListener("input", async (e) => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  let size = e.target.value;
+  chrome.storage.sync.set({ size });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: changePageFontSize,
   });
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
+function changePageFontSize() {
+  chrome.storage.sync.get("size", ({ size }) => {
+    document.body.style.fontSize = size + "%";
   });
 }
+
+let speech = new SpeechSynthesisUtterance();
+
+// Set Speech Language
+speech.lang = "en";
+
+let voices = []; // global array of available voices
+
+window.speechSynthesis.onvoiceschanged = () => {
+  // Get List of Voices
+  voices = window.speechSynthesis.getVoices();
+
+  // Initially set the First Voice in the Array.
+  speech.voice = voices[0];
+}
+
+start.addEventListener("click", () => {
+  // Set the text property with the value of the textarea
+  speech.text = document.querySelector("textarea").value;
+
+  // Start Speaking
+  window.speechSynthesis.speak(speech);
+});
+
+pause.addEventListener("click", () => {
+  // Pause the speechSynthesis instance
+  window.speechSynthesis.pause();
+});
+
+resume.addEventListener("click", () => {
+  // Resume the paused speechSynthesis instance
+  window.speechSynthesis.resume();
+});
+
+cancel.addEventListener("click", () => {
+  // Cancel the speechSynthesis instance
+  window.speechSynthesis.cancel();
+});
